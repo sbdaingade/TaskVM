72ebbbfe5905e367c1b0e167f7e3a7d8609c7fe4
Sachin Daingade <sbdaingade@gmail.com>
2022-05-06 19:47:15 +0530

    unit testcases added

--- Virtusa-IOS-Developer-Assignment/Virtusa-IOS-Developer-AssignmentTests/Virtusa_IOS_Developer_AssignmentTests.swift
+++ Virtusa-IOS-Developer-Assignment/Virtusa-IOS-Developer-AssignmentTests/Virtusa_IOS_Developer_AssignmentTests.swift
@@ -9,7 +9,7 @@ import XCTest
 @testable import Virtusa_IOS_Developer_Assignment
 
 class Virtusa_IOS_Developer_AssignmentTests: XCTestCase {
-
+    let peopleViewModel = PeopleViewModel()
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -30,4 +30,50 @@ class Virtusa_IOS_Developer_AssignmentTests: XCTestCase {
         }
     }
 
+    func testGetPeople() throws {
+        let expec = expectation(description: "test get people request")
+        peopleViewModel.input.action.value =  .getPeoples
+       
+        peopleViewModel.output.requestState.next {[unowned self] state in
+            switch state {
+            case .idle,.none,.progress:
+                break
+            case .finished:
+                print("Number of people: \(self.peopleViewModel.output.arrayOfPeople.value?.count ?? 0)")
+                expec.fulfill()
+            case .failWithError(let error):
+                XCTFail(String(format: "fail to get people: \n %@", error.localizedDescription))
+                expec.fulfill()
+            }
+        }
+        waitForExpectations(timeout: 12.0) { error in
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    func testGetRoom() throws {
+        let expec = expectation(description: "test get room request")
+        peopleViewModel.input.action.value =  .getRooms
+       
+        peopleViewModel.output.requestState.next {[unowned self] state in
+            switch state {
+            case .idle,.none,.progress:
+                break
+            case .finished:
+                print("Number of rooms: \(self.peopleViewModel.output.arrayOfRooms.value?.count ?? 0)")
+                expec.fulfill()
+            case .failWithError(let error):
+                XCTFail(String(format: "fail to get rooms: \n %@", error.localizedDescription))
+                expec.fulfill()
+            }
+        }
+        waitForExpectations(timeout: 12.0) { error in
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+            }
+        }
+    }
+    
 }
